874cb888fd256f04717fe989a82f79ad684c5dca
Olivier Cornu <o.cornu@gmail.com>
2009-04-26 15:21:24 +0200

    Remove warnings unless the -v (verbose) parameter is used

--- mkdoc.sh
+++ mkdoc.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 SRC=$PWD/src
 DOC=$PWD/doc
+echo Build doc in $DOC
+
 rm -rf $DOC
 cd utils/jsdoc-toolkit
-java -jar jsrun.jar app/run.js -a -t=templates/webmonkey -d=$DOC \
-	$SRC/modules/lib/*.js \
-	$SRC/modules/*.js \
-	$SRC/chrome/content/lib/*.js \
-	$SRC/chrome/content/*.js \
-	$SRC/components/*.js
+
+make() {
+	java -jar jsrun.jar app/run.js -a -t=templates/webmonkey -d=$DOC \
+		$SRC/modules/lib/*.js \
+		$SRC/modules/*.js \
+		$SRC/chrome/content/lib/*.js \
+		$SRC/chrome/content/*.js \
+		$SRC/components/*.js
+}
+
+if [ "$1" == "-v" ]; then
+	make
+else
+	# remove warnings
+	make | grep -v -e "^>> WARNING:"
+fi
