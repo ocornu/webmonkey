1dadb6fc0153b88c11ba104bd3d3123f1c12c962
Olivier Cornu <o.cornu@gmail.com>
2009-08-29 18:10:25 +0200

    Cosmetic: change a few build.sh names.

--- build.sh
+++ build.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 # Set up variables
-GMBASE=`grep "<em:version>" src/install.rdf | sed -r "s/^\s*<em:version>(.+)<\/em:version>\s*$/\\1/"`
-GMNAME=webmonkey
-GMVER=$GMBASE
+NAME=webmonkey
+VERSION=`grep "<em:version>" src/install.rdf | sed -r "s/^\s*<em:version>(.+)<\/em:version>\s*$/\\1/"`
+BUILD=$VERSION
 if [ "$1" != "-r" ]; then
-	GMBUILD=`date +"%Y%m%d"`
-	GMVER="$GMVER.$GMBUILD.${1-0}"
+	DATE=`date +"%Y%m%d"`
+	BUILD="$BUILD.$DATE.${1-0}"
 fi
-GMXPI="$GMNAME-$GMVER.xpi"
+XPI="$NAME-$BUILD.xpi"
 
 # Copy base structure to a temporary build directory and change to it
 echo "Creating working directory ..."
@@ -21,17 +21,17 @@ cp -r chrome.manifest install.rdf license.txt \
 cd ../build
 
 echo "Gathering all locales into chrome.manifest ..."
-GMLOC=\"en-US\"
+LOCALES=\"en-US\"
 for entry in chrome/locale/*; do
   entry=`basename $entry`
   if [ $entry != en-US ]; then
-    echo "locale  $GMNAME  $entry  chrome/locale/$entry/" >> chrome.manifest
-    GMLOC=$GMLOC,\ \"$entry\"
+    echo "locale  $NAME  $entry  chrome/locale/$entry/" >> chrome.manifest
+    LOCALES=$LOCALES,\ \"$entry\"
   fi
 done
 
 echo "Patching install.rdf version ..."
-sed "s!<em:version>.*</em:version>!<em:version>$GMVER</em:version>!" \
+sed "s!<em:version>.*</em:version>!<em:version>$BUILD</em:version>!" \
   install.rdf > install.rdf.tmp
 mv install.rdf.tmp install.rdf
 
@@ -40,8 +40,8 @@ find . -depth -name '.svn' -exec rm -rf "{}" \;
 find . -depth -name '*~' -exec rm -rf "{}" \;
 find . -depth -name '#*' -exec rm -rf "{}" \;
 
-echo "Creating $GMXPI ..."
-zip -qr9X "../$GMXPI" *
+echo "Creating $XPI ..."
+zip -qr9X "../$XPI" *
 
 echo "Cleaning up temporary files ..."
 cd ..
